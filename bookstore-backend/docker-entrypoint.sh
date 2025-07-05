a7b5b72b1a606264d839c9aea81657ec0531a32a
Faheem <faheemullah952@gmail.com>
2025-07-05 17:18:52 +0500

    Update in docker docker-compose remove postgres image for now

--- bookstore-backend/docker-entrypoint.sh
+++ bookstore-backend/docker-entrypoint.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 # Wait for PostgreSQL
 /app/wait-for-postgres.sh
 
-echo "🛠️ -- Running Alembic migrations..."
-alembic upgrade head
+#echo "🛠️ -- Running Alembic migrations..."
+#alembic upgrade head
 
 echo "🚀 -- Starting FastAPI app..."
 exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
